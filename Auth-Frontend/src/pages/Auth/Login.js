c0949c9261f9e7c145b2b9b91deceeb321c3b5a7
naman <naman.v@hashtechy.com>
2023-09-15 12:07:30 +0530

    intial commit | app setup

---
+++ Auth-Frontend/src/pages/Auth/Login.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Form, Button, Col, Container, Alert } from 'react-bootstrap';
+import { Formik, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object().shape({
+  username: Yup.string()
+    .required('Username is required'),
+  password: Yup.string()
+    .required('Password is required'),
+});
+
+const initialValues = {
+  username: '',
+  password: '',
+};
+
+const Login = () => {
+  const handleSubmit = (values, { setSubmitting }) => {
+    // You can handle the login logic here, e.g., sending a request to your server.
+    console.log('Submitted values:', values);
+    setSubmitting(false);
+  };
+
+  return (
+    <Container>
+      <h2>Login</h2>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ handleSubmit, isSubmitting }) => (
+          <Form noValidate onSubmit={handleSubmit}>
+            <Form.Row>
+              <Form.Group as={Col} controlId="username">
+                <Form.Label>Username</Form.Label>
+                <Field
+                  type="text"
+                  name="username"
+                  as={Form.Control}
+                  placeholder="Enter your username"
+                />
+                <ErrorMessage name="username" component="div" className="text-danger" />
+              </Form.Group>
+            </Form.Row>
+
+            <Form.Row>
+              <Form.Group as={Col} controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Field
+                  type="password"
+                  name="password"
+                  as={Form.Control}
+                  placeholder="Enter your password"
+                />
+                <ErrorMessage name="password" component="div" className="text-danger" />
+              </Form.Group>
+            </Form.Row>
+
+            <Button type="submit" disabled={isSubmitting}>
+              Login
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Container>
+  );
+};
+
+export default Login;
